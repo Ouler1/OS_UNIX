2ec74cdd43b82f76312bc235d41339a4c724ae1a
Ouler1 <33118713+Ouler1@users.noreply.github.com>
2019-03-02 21:08:52 +0500

    -

--- task2.c
+++ task2.c
@@ -4,8 +4,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
 int main(int argc, char *argv[]) {
 	int fd = open(argv[1], O_WRONLY|O_TRUNC|O_CREAT);
+	//Обработка ошибок с файлом
 	if (fd == -1) {
 		char msg[100] = "FILE ERROR\n";
 		printf("%s", msg);
@@ -13,8 +15,11 @@ int main(int argc, char *argv[]) {
 	}
 	while (1) {
 		char buf;
+		//Считываем следующий символ из входного потока
 		int cod = scanf("%c", &buf);
+		//Если считывать нечего, выходим
 		if (cod == -1) break;
+		//Если не нулевой байт, пишем на диск, иначе пропускаем операйией lseek.
 		if (buf != 0) write(fd, &buf, 1);
 		else lseek(fd, 1, SEEK_CUR);
 	}
